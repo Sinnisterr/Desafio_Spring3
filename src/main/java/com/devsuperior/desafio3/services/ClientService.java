b2a9bf401ec255488f2d9332f6cedc7cb3c43d54
Willian Bruno <mestrebruno@gmail.com>
2025-06-07 17:39:49 -0300

    Criado a minha excessao personalizada na camada servico findbyid

--- src/main/java/com/devsuperior/desafio3/services/ClientService.java
+++ src/main/java/com/devsuperior/desafio3/services/ClientService.java
@@ -3,10 +3,12 @@ package com.devsuperior.desafio3.services;
 import com.devsuperior.desafio3.dto.ClientDTO;
 import com.devsuperior.desafio3.entities.Client;
 import com.devsuperior.desafio3.repositories.ClientRepository;
+import com.devsuperior.desafio3.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,12 +19,14 @@ public class ClientService {
     @Autowired
     private ClientRepository clientRepository;
 
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
-        Client client = clientRepository.findById(id).get();
-        ClientDTO dto = new ClientDTO(client);
-        return dto;
+        Client client = clientRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
+        return new ClientDTO(client);
     }
 
+    @Transactional(readOnly = true)
     public Page<ClientDTO> findAll(Pageable pageable) {
         Page<Client> clients = clientRepository.findAll(pageable);
         return clients.map(client -> new ClientDTO(client));
